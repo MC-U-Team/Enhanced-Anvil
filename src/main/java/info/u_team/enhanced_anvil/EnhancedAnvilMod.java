98aebc417d8533d1b7fe88c025abf447b112ac70
HyCraftHD <hycrafthd@live.de>
2019-10-19 17:45:10 +0200

    Add mod class

---
+++ src/main/java/info/u_team/enhanced_anvil/EnhancedAnvilMod.java
@@ -0,0 +1,10 @@
+package info.u_team.enhanced_anvil;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(EnhancedAnvilMod.MODID)
+public class EnhancedAnvilMod {
+	
+	public static final String MODID = "enhancedanvil";
+	
+}
