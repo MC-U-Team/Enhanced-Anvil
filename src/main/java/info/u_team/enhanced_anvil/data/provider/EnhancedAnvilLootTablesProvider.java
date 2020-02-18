8a9ec9fa81d76a1cb4edd7ab8824331a889a9b32
HyCraftHD <hycrafthd@live.de>
2020-02-18 14:31:45 +0100

    Fix loot tables provider

--- src/main/java/info/u_team/enhanced_anvil/data/provider/EnhancedAnvilLootTablesProvider.java
+++ src/main/java/info/u_team/enhanced_anvil/data/provider/EnhancedAnvilLootTablesProvider.java
@@ -2,29 +2,16 @@ package info.u_team.enhanced_anvil.data.provider;
 
 import static info.u_team.enhanced_anvil.init.EnhancedAnvilBlocks.*;
 
-import java.io.IOException;
 import java.util.function.BiConsumer;
 
 import info.u_team.u_team_core.data.*;
-import net.minecraft.data.DirectoryCache;
 import net.minecraft.util.ResourceLocation;
-import net.minecraft.world.storage.loot.*;
+import net.minecraft.world.storage.loot.LootTable;
 
 public class EnhancedAnvilLootTablesProvider extends CommonLootTablesProvider {
 	
 	public EnhancedAnvilLootTablesProvider(GenerationData data) {
 		super(data);
-	}
-	
-	@Override
-	public void act(DirectoryCache cache) throws IOException {
-		registerLootTables((id, lootTable) -> {
-			try {
-				write(cache, LootTableManager.toJson(lootTable), path.resolve(id.getNamespace()).resolve("loot_tables").resolve(id.getPath() + ".json"));
-			} catch (IOException ex) {
-				LOGGER.error(marker, "Could not write data.", ex);
-			}
-		});
 	}
 	
 	@Override
