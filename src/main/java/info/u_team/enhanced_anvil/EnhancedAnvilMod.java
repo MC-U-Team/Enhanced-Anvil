9814bd7edbc95aeb1221172cb9db7aff01d73a39
HyCraftHD <hycrafthd@live.de>
2020-02-19 03:09:33 +0100

    Add check for sign

--- src/main/java/info/u_team/enhanced_anvil/EnhancedAnvilMod.java
+++ src/main/java/info/u_team/enhanced_anvil/EnhancedAnvilMod.java
@@ -1,10 +1,15 @@
 package info.u_team.enhanced_anvil;
 
+import info.u_team.u_team_core.util.verify.JarSignVerifier;
 import net.minecraftforge.fml.common.Mod;
 
 @Mod(EnhancedAnvilMod.MODID)
 public class EnhancedAnvilMod {
 	
 	public static final String MODID = "enhancedanvil";
+	
+	public EnhancedAnvilMod() {
+		JarSignVerifier.checkSigned(MODID);
+	}
 	
 }
