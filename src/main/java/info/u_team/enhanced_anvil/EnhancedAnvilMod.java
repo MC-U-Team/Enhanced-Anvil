391277d8a3a12f514ad4bb32f98a9199ccfb47a9
HyCraftHD <hycrafthd@live.de>
2020-06-27 00:14:20 +0200

    Add deferred register to container types

--- src/main/java/info/u_team/enhanced_anvil/EnhancedAnvilMod.java
+++ src/main/java/info/u_team/enhanced_anvil/EnhancedAnvilMod.java
@@ -1,6 +1,6 @@
 package info.u_team.enhanced_anvil;
 
-import info.u_team.enhanced_anvil.init.EnhancedAnvilBlocks;
+import info.u_team.enhanced_anvil.init.*;
 import info.u_team.u_team_core.util.registry.BusRegister;
 import info.u_team.u_team_core.util.verify.JarSignVerifier;
 import net.minecraftforge.fml.common.Mod;
@@ -17,6 +17,7 @@ public class EnhancedAnvilMod {
 	
 	private void register() {
 		BusRegister.registerMod(EnhancedAnvilBlocks::register);
+		BusRegister.registerMod(EnhancedAnvilContainerTypes::register);
 	}
 	
 }
