e00e2c001099be4701bac072734ff5d93275e0ed
HyCraftHD <hycrafthd@live.de>
2020-06-27 00:13:01 +0200

    Register the anvil blocks with the deferred register

--- src/main/java/info/u_team/enhanced_anvil/init/EnhancedAnvilBlocks.java
+++ src/main/java/info/u_team/enhanced_anvil/init/EnhancedAnvilBlocks.java
@@ -2,16 +2,17 @@ package info.u_team.enhanced_anvil.init;
 
 import info.u_team.enhanced_anvil.EnhancedAnvilMod;
 import info.u_team.enhanced_anvil.block.EnhancedAnvilBlock;
-import info.u_team.u_team_core.util.registry.BlockDeferredRegister;
+import info.u_team.u_team_core.util.registry.*;
+import net.minecraft.item.BlockItem;
 import net.minecraftforge.eventbus.api.IEventBus;
 
 public class EnhancedAnvilBlocks {
 	
 	public static final BlockDeferredRegister BLOCKS = BlockDeferredRegister.create(EnhancedAnvilMod.MODID);
 	
-	public static final EnhancedAnvilBlock ENHANCED_ANVIL = new EnhancedAnvilBlock("enhanced_anvil");
-	public static final EnhancedAnvilBlock CHIPPED_ENHANCED_ANVIL = new EnhancedAnvilBlock("chipped_enhanced_anvil");
-	public static final EnhancedAnvilBlock DAMAGED_ENHANCED_ANVIL = new EnhancedAnvilBlock("damaged_enhanced_anvil");
+	public static final BlockRegistryObject<EnhancedAnvilBlock, BlockItem> ENHANCED_ANVIL = BLOCKS.register("enhanced_anvil", EnhancedAnvilBlock::new);
+	public static final BlockRegistryObject<EnhancedAnvilBlock, BlockItem> CHIPPED_ENHANCED_ANVIL = BLOCKS.register("chipped_enhanced_anvil", EnhancedAnvilBlock::new);
+	public static final BlockRegistryObject<EnhancedAnvilBlock, BlockItem> DAMAGED_ENHANCED_ANVIL = BLOCKS.register("damaged_enhanced_anvil", EnhancedAnvilBlock::new);
 	
 	public static void register(IEventBus bus) {
 		BLOCKS.register(bus);
