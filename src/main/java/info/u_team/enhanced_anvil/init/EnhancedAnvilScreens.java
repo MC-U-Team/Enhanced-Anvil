059b51f359ccd4c453aad73895bde4c77edb2a59
HyCraftHD <hycrafthd@live.de>
2020-06-27 00:17:38 +0200

    Fix the anvil screens

--- src/main/java/info/u_team/enhanced_anvil/init/EnhancedAnvilScreens.java
+++ src/main/java/info/u_team/enhanced_anvil/init/EnhancedAnvilScreens.java
@@ -2,7 +2,7 @@ package info.u_team.enhanced_anvil.init;
 
 import info.u_team.enhanced_anvil.EnhancedAnvilMod;
 import info.u_team.enhanced_anvil.screen.EnhancedAnvilScreen;
-import net.minecraft.client.gui.ScreenManager;
+import info.u_team.u_team_core.util.registry.*;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
@@ -14,7 +14,9 @@ public class EnhancedAnvilScreens {
 	
 	@SubscribeEvent
 	public static void register(FMLClientSetupEvent event) {
-		ScreenManager.registerFactory(EnhancedAnvilContainerTypes.ENHANCED_ANVIL, EnhancedAnvilScreen::new);
+		MainThreadWorker.run(() -> {
+			ClientRegistry.registryScreen(EnhancedAnvilContainerTypes.ENHANCED_ANVIL, EnhancedAnvilScreen::new);
+		});
 	}
 	
 }
